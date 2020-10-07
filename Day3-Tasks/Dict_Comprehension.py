8ec0a8cf1abaf4a5896cfabe2b418475b702cc7b
nvk1152 <nvk1152@gmail.com>
2020-10-07 19:13:33 +0530

    day3 tasks

---
+++ Day3-Tasks/Dict_Comprehension.py
@@ -0,0 +1,17 @@
+# Using Dict Comprehension to print students with 'cse' department
+
+# Student details
+students = [
+    ("Vamsi", "CSE"),
+    ("Raju", "ECE"),
+    ("Mike", "EEE"),
+    ("Sam", "CSE"),
+    ("Bob", "CSE")
+]
+
+# Mapping student details to name using dictionary
+student_dep = {user[0] : user for user in students}
+
+for name, det in student_dep.items():
+    if "CSE" in det :
+        print(name)
---
+++ Day3-Tasks/Range.py
@@ -0,0 +1,5 @@
+# Using range to print multiples of 3 from 1 to 33
+i = 1
+# Rage starts from 1 ends at 33
+for i in range(1,34): 
+    print(i * 3, end = ', ')
